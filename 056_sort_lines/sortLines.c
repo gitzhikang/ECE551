d3dbfcd7ff258314f94523c812a0001f63fac4be
zs181 <zs181@duke.edu>
2024-10-07 19:09:55 -0400

    did 056

--- 056_sort_lines/sortLines.c
+++ 056_sort_lines/sortLines.c
@@ -13,9 +13,45 @@ int stringOrder(const void * vp1, const void * vp2) {
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
+void readFromLineAndsort(FILE * f);
 
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
+  if (argc == 1) {
+    readFromLineAndsort(stdin);
+  }
+
+  for (int i = 1; i < argc; i++) {
+    FILE * f = fopen(argv[i], "r");
+    if (f == NULL) {
+      fprintf(stderr, "can't open file");
+      exit(EXIT_FAILURE);
+    }
+    readFromLineAndsort(f);
+    fclose(f);
+  }
 
   return EXIT_SUCCESS;
+}
+
+void readFromLineAndsort(FILE * f) {
+  // an array of string
+  char ** lines = NULL;
+  char * curr = NULL;
+  size_t linecap;
+  size_t i = 0;
+  while (getline(&curr, &linecap, f) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  //please free lines
+  sortData(lines, i);
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
 }
