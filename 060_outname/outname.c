8206ece9021cd82abbd54035559e8effb996c745
zs181 <zs181@duke.edu>
2024-10-09 14:08:15 -0400

    060

--- 060_outname/outname.c
+++ 060_outname/outname.c
@@ -1,8 +1,14 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  size_t len = strlen(inputName);
+  char * outName = malloc(sizeof(*outName) * (len + 7));
+  strcpy(outName, inputName);
+  strcat(outName, ".counts");
+  return outName;
 }
